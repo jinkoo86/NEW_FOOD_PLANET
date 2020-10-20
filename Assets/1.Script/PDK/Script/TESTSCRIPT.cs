933970ef9382ede4e050ba47698be2b635f026ac
dongkeon95 <suntan1234@gmail.com>
2020-10-20 17:59:22 +0900

    20201020_2
    여러가지

--- Assets/1.Script/PDK/Script/TESTSCRIPT.cs
+++ Assets/1.Script/PDK/Script/TESTSCRIPT.cs
@@ -1,12 +1,35 @@
-﻿using System.Collections;
+﻿using Mono.Data.Sqlite;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class TESTSCRIPT : MonoBehaviour {
-    GameObject targetObject;
-    // Start is called before the first frame update
+
     void Start() {
-        targetObject = GameObject.FindWithTag("COUNTER");
+        {
+            string conn = "URI=file:" + Application.dataPath + "/StreamingAssets/DB.db"; //Path to database.
+            IDbConnection dbconn;
+            dbconn = (IDbConnection)new SqliteConnection(conn);
+            dbconn.Open(); //Open connection to the database.
+
+            IDbCommand dbcmd = dbconn.CreateCommand();
+            string sqlQuery = "SELECT *" + "FROM MyEquip";
+            dbcmd.CommandText = sqlQuery;
+            IDataReader reader = dbcmd.ExecuteReader();
+            while (reader.Read()) {
+                string EquipName = reader.GetString(0);
+                int EquipLevel = reader.GetInt32(1);
+                //ID,NAME,AGE,ADDRESS,SALARY
+                Debug.Log("EquipName= " + EquipName + "  EquipLevel =" + EquipLevel);
+            }
+            reader.Close();
+            reader = null;
+            dbcmd.Dispose();
+            dbcmd = null;
+            dbconn.Close();
+            dbconn = null;
+        }
     }
 
     // Update is called once per frame
